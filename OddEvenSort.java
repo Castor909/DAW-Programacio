d5f504d925ed68da7885b6cddaf2f225384d96e2
Stepan Andreev <sandreev@cifpfbmoll.eu>
2024-12-12 16:18:20 +0100

    Create OddEvenSort.java
    https://www.codewars.com/kata/580755730b5a77650500010c

---
+++ OddEvenSort.java
@@ -0,0 +1,9 @@
+public class OddEvenSort {
+  public static String sortMyString(String s) {
+    String oddLine="", evenLine="";
+    for(int i=0;i<s.length();i++)         //go by each letter
+      if(i%2==0)  evenLine+=s.charAt(i);  //put letters at even and odd indexes at their corresponding strings
+      else oddLine+=s.charAt(i);
+    return evenLine+" "+oddLine;          //combine strings with a space between
+  }
+}
