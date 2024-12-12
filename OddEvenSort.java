b30a6d47b8814981c9886e33eeb3b414c5d5022f
Stepan Andreev <sandreev@cifpfbmoll.eu>
2024-12-12 16:21:12 +0100

    Update OddEvenSort.java

--- OddEvenSort.java
+++ OddEvenSort.java
@@ -2,7 +2,7 @@ public class OddEvenSort {
   public static String sortMyString(String s) {
     String oddLine="", evenLine="";
     for(int i=0;i<s.length();i++)         //go by each letter
-      if(i%2==0)  evenLine+=s.charAt(i);  //put letters at even and odd indexes at their corresponding strings
+      if(i%2==0)  evenLine+=s.charAt(i);  //put letters at even and odd indexes to their corresponding strings
       else oddLine+=s.charAt(i);
     return evenLine+" "+oddLine;          //combine strings with a space between
   }
