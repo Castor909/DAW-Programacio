1f0a60a7aca5edd3f1fc3c15731adaa8cd7bbc69
Stepan Andreev <sandreev@cifpfbmoll.eu>
2025-01-28 16:16:54 +0100

    Update ColouredTriangles.java

--- ColouredTriangles.java
+++ ColouredTriangles.java
@@ -6,35 +6,41 @@ public class Kata {
 // R+R=164
 // G+G=142
 // B+B=132
-  
+
 // R+G=153
 // R+B=148
 // G+B=137
-  
-  public static char triangle(final String row) {
-    if(row.length()==1) return row.charAt(0);
-    if(row.length()==2) {
-      char[] twoChars = row.toCharArray();
-      int sumChars = twoChars[0]+twoChars[1];
-      switch(sumChars)  {
-          case 164:
-          case 137:
-            return 'R';
-          case 142:
-          case 148:
-            return 'G';
-          case 132:
-          case 153:
-            return 'B';      
-      }
+
+    public static char triangle(final String row) {
+        switch (row.length()) {
+            case 1 -> {
+                return row.charAt(0);
+            }
+            case 2 -> {
+                char[] twoChars = row.toCharArray();
+                int sumChars = twoChars[0] + twoChars[1];
+                switch (sumChars) {
+                    case 164:
+                    case 137:
+                        return 'R';
+                    case 142:
+                    case 148:
+                        return 'G';
+                    case 132:
+                    case 153:
+                        return 'B';
+                }
+            }
+            default -> {
+                String newLine = "";
+                for (int i = 1; i < row.length(); i++)
+                    newLine += triangle(
+                            (Character.toString(row.charAt(i - 1)))
+                                    + (Character.toString(row.charAt(i)))
+                    );
+                return triangle(newLine);
+            }
+        }
+        return 0;
     }
-    String newLine = "";
-    for(int i=1;i<row.length();i++)
-      newLine+=triangle(
-      (Character.toString(row.charAt(i-1)))
-      +
-      (Character.toString(row.charAt(i)))
-    );
-    return triangle(newLine);
-  }
 }
